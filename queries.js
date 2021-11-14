e5b09a89731ea127b0f02b43ec62da7797fd7f1c
Szabolcs Meszaros <93373750+skandar98@users.noreply.github.com>
2021-11-14 13:06:03 +0100

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,6 +1,6 @@
 async function TreatQuery(disease) {
   var treat_query = 'SELECT ?treatlabel	WHERE {wd:' + disease + ' wdt:P2176 ?treat. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?treatLabel';
-  console.log(treat_query);
+  console.log(treat_query); //for debugging
   return treat_query;
 }
 
@@ -13,7 +13,7 @@ async function SymptQuery(disease) {
 
 async function GeneQuery(disease) {
   var gene_query = 'SELECT ?geneLabel WHERE {wd:' + disease + ' wdt:P5572 ?gene. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?geneLabel';
-  console.log(gene_query);
+  console.log(gene_query); //for debugging
   return gene_query;
 }
 
@@ -26,7 +26,7 @@ async function Retrieve(query) {
     var url = wdk.sparqlQuery(query);
     var response = await fetch(url);
     var results = await response.json();
-	var final = wdk.simplify.sparqlResults(results);
-	console.log(final)
+    var final = wdk.simplify.sparqlResults(results);
+    console.log(final) //for debugging
     return final;
 }
