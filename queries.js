0f1f6e1e014c9de170a70de2a460c954e213077a
Szabolcs Meszaros <93373750+skandar98@users.noreply.github.com>
2021-11-14 13:04:46 +0100

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,57 +1,32 @@
 async function TreatQuery(disease) {
-  var treat_query = "
-	SELECT ?treatlabel
-	WHERE 
-	{
-  	wd:"+disease" wdt:P2176 ?treat.     
-  	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
-	}
-	GROUP BY ?treatLabel";
+  var treat_query = 'SELECT ?treatlabel	WHERE {wd:' + disease + ' wdt:P2176 ?treat. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?treatLabel';
+  console.log(treat_query);
   return treat_query;
 }
 
+//treatQuery('Q181923');
+
 async function SymptQuery(disease) {
-  var sympt_query = "
-	SELECT ?symptLabel
-	WHERE 
-	{
- 	wd:"+disease"dt:P780 ?sympt.        
-  	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
-	}GROUP BY ?symptLabel";
+  var sympt_query = 'SELECT ?symptLabel	WHERE {wd:' + disease + 'dt:P780 ?sympt.	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?symptLabel';
   return sympt_query;
 }
 
 async function GeneQuery(disease) {
-  var gene_query = "
-	SELECT ?geneLabel
-	WHERE 
-	{
- 	wd:"+disease" wdt:P5572 ?gene.        
-  	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
-	}GROUP BY ?geneLabel";
+  var gene_query = 'SELECT ?geneLabel WHERE {wd:' + disease + ' wdt:P5572 ?gene. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?geneLabel';
+  console.log(gene_query);
   return gene_query;
 }
 
 async function StructQuery(disease) {
-  var struct_query = "
-	SELECT ?structureLabel
-	WHERE
-	{
-  	wd:Q202387 wdt:P2293 ?gene.
-  	?gene wdt:P5572 ?structure.
-  	VALUES (?regions) {(wd:Q1620186) (wd:Q1073)}
-  	?structure wdt:P31|wdt:P2791 ?regions.
-  
-  	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
-	}	
-	GROUP BY ?structureLabel";
+  var struct_query = 'SELECT ?structureLabel WHERE {wd:' + disease + ' wdt:P2293 ?gene.	?gene wdt:P5572 ?structure.	VALUES (?regions) {(wd:Q1620186) (wd:Q1073)} ?structure wdt:P31|wdt:P2791 ?regions. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } } GROUP BY ?structureLabel';
   return struct_query; 
 }
 
-async function Retreive(query) {
+async function Retrieve(query) {
     var url = wdk.sparqlQuery(query);
     var response = await fetch(url);
     var results = await response.json();
-    var final = wdk.simplify.sparqlResults(results);
+	var final = wdk.simplify.sparqlResults(results);
+	console.log(final)
     return final;
 }
